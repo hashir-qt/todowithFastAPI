ea484d9b2c4af4ed701bf93725184371f04913b3
Hashir Irshad <khanjarquantum@gmail.com>
2025-08-24 21:15:40 +0500

    basic todo App

--- main.py
+++ main.py
@@ -23,9 +23,43 @@ def get_todo(todo_id: int):
         if todo ['todo_id'] == todo_id:
             return {'result': todo}
         
+
 @api.get('/todos')
 def get_todos(first_n: int = None):
     if first_n:
         return all_todos[:first_n]
     else:
         return all_todos 
+
+
+@api.post('/todos')
+def create_todo(todo: dict):
+    new_todo_id = max(todo['todo_id'] for todo in all_todos) + 1
+
+    new_todo = {
+        'todo_id': new_todo_id,
+        'todo_name': todo['todo_name'],
+        'todo_description': todo['todo_description']
+    }
+
+    all_todos.append(new_todo)
+    return new_todo
+
+
+@api.put('/todos/{todo_id}')
+def update_todo(todo_id: int, updated_todo: dict):
+    for todo in all_todos:
+     if todo['todo_id'] == todo_id:
+        todo['todo_name'] = updated_todo['todo_name']
+        todo['todo_description'] = updated_todo['todo_description']
+        return todo
+    return "Error, NOT FOUND"
+
+
+@api.delete('/todos/{todo_id}')
+def delete_todo(todo_id: int):
+    for index, todo in enumerate(all_todos):
+        if todo['todo_id'] == todo_id:
+            deleted_todo = all_todos.pop(index)
+            return deleted_todo
+    return "Error, NOT FOUND"
